c702e35f64df6b0ed5b393b56282cac0dd4a82d4
Reinhold <8436725+reikoch@users.noreply.github.com>
2023-10-25 15:43:26 +0200

    split off constant columns

--- prepare.R
+++ prepare.R
@@ -1,7 +1,7 @@
 library(readxl)
 library(philentropy)
 
-distinctness <- function(x) philentropy::H(table(x)/length(x)) / log(length(x), 2)
+distinctness <- function(x) philentropy::H(table(x, useNA = 'ifany')/length(x)) / log(length(x), 2)
 
 # read in data if not already present
 if (!exists('dat0')) {
@@ -9,3 +9,8 @@ if (!exists('dat0')) {
   dat0 <- readxl::read_xlsx(tf)
 }
 
+# determine constant columns, remove them to "general" dataframe
+xx <- sapply(dat0, distinctness)
+general <- unique(dat0[,names(xx[xx==0])])
+
+dat1 <- dat0[, setdiff(names(dat0), names(general))]
